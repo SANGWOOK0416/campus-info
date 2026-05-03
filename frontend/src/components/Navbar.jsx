4d7a7f4e2cbdc98ceb1fce356afbe6420b31e70f
snowsoomin <snowsoomin@gmail.com>
2026-05-03 09:33:31 +0900

    feat: 로그인 여부에 따른 로그인/로그아웃 버튼 구현

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Link 추가
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../assets/logo.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
   const closeMenu = () => setIsOpen(false);
@@ -21,7 +22,7 @@ const Navbar = () => {
           <Link to="/majorcommunity"><span>전공 게시판</span></Link>
           <Link to="/gradecommunity"><span>학년 게시판</span></Link>
           <Link to="/mypage"><span>마이 페이지</span></Link>
-          <Link to="/login"><button className="loginBtn">Login</button></Link>
+          <Link to="/login"><button className="loginBtn">{isLoggedIn ? 'Logout' : 'Login'}</button></Link>
         </div>
 
         <button className="hamburger" onClick={toggleMenu}>
@@ -40,7 +41,8 @@ const Navbar = () => {
             <Link to="/mypage" onClick={closeMenu}><span>마이 페이지</span></Link>
           </div>
           <Link to="/login" onClick={closeMenu}>
-            <button className="mobileLoginBtn">Login</button>
+            {/* 로그인 여부에 따라 텍스트 변경 */}
+            <button className="mobileLoginBtn">{isLoggedIn ? 'Logout' : 'Login'}</button>
           </Link>
         </div>
       </div>
